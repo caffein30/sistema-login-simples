79f8a79c06e06f5014d4b8704b40ae6485d5e528
kafein <kafein@DESKTOP-FQTMOOT>
2023-06-12 13:23:47 -0300

    Adicionar arquivos de projeto.

---
+++ senha/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace login
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string UsuarioSistema = "gabriel";
+            string SenhaSistema = "123";
+
+            string UsuarioInserido;
+            string SenhaInserida;
+
+            int tentativas = 0;
+
+
+            do
+            {
+                Console.Clear();
+                Console.Write("Digite o nome de usuário: ");
+                UsuarioInserido = Console.ReadLine();
+                Console.Write("Digite a senha:");
+                SenhaInserida = Console.ReadLine();
+
+                tentativas++;
+
+            } while (UsuarioInserido != UsuarioSistema && SenhaInserida != SenhaSistema && tentativas < 3);
+
+            
+
+
+            if (UsuarioInserido == UsuarioSistema && SenhaInserida == SenhaSistema)
+            {
+                Console.Clear();
+                Console.WriteLine("Seja bem vindo ao sistema Sr. gabriel");
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine($"Usuário ou senha inválida, você usou {tentativas} /3 tentativas");
+            }
+            Console.ReadKey();
+        }
+    }
+}
